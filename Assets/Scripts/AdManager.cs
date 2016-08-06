607b5da06b870eca4b2ea369a92706c39a095193
tundetunde <tundetunde000@gmail.com>
2016-08-06 02:39:02 +0100

    Admob Integration

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,38 +1,41 @@
-﻿using UnityEngine;
+﻿using GoogleMobileAds.Api;
+using UnityEngine;
 
 public class AdManager : MonoBehaviour {
     public static AdManager Instance { set; get; }
-    public string bannerId;
     public string videoAd;
     private void Start()
     {
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
-        //#if UNITY_EDITOR
-        //#elif UNITY_ANDROID
-        //            Admob.Instance().initAdmob(bannerId, videoAd);
-        //            Admob.Instance().loadInterstitial();
-        //            Admob.Instance().setTesting(true);
-        //#endif
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-6044705985167929/9665428898";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-6044705985167929/9665428898";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
 
+        AdRequest request = new AdRequest.Builder()
+        .AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
+        // My test device.
+        .Build();
+
+
+        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
     }
 
     public void ShowBanner()
     {
-        //#if UNITY_EDITOR
-        //#elif UNITY_ANDROID
-        //            Admob.Instance().showBannerRelative(AdSize.Banner, AdPosition.BOTTOM_CENTER, 5);
-        //#endif
+        
     }
 
     public void ShowVideo()
     {
-        //#if UNITY_EDITOR
-        //        if (Admob.Instance().isInterstitialReady())
-        //            Admob.Instance().showInterstitial();
-        //#elif UNITY_ANDROID
-        //#endif
+       
 
     }
 }
