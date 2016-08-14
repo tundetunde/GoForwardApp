671095468174cd7b772a11f1a2ce62a86d41f266
Jesz <adajess01@gmail.com>
2016-08-14 01:30:31 +0100

    Another one

---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseButton : MonoBehaviour {
+
+	public Image img;
+	public Sprite play, pause;
+	public bool click;
+	
+	// Use this for initialization
+	void Start () {
+		click = true;
+	}
+	
+	public void Halt () {
+		if (Time.timeScale == 1)
+         {
+             Time.timeScale = 0;
+			 img.sprite = play;
+			 click = false;
+         }
+         else
+         {
+             Time.timeScale = 1;
+			 img.sprite = pause;
+			 click = true;
+         }
+	}
+}
