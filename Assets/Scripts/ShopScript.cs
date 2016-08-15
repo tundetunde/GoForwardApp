f010e2fe8bf0e135e2c89ca0151bcd592c4681b7
tundetunde <tundetunde000@gmail.com>
2016-08-15 16:06:41 +0100

    Shop Updated

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ShopScript : MonoBehaviour {
     public GameObject menu;
     public static ShopScript Instance { set; get; }
+    public Text text;
 
     public enum COLOURS
     {
@@ -17,34 +19,102 @@ public class ShopScript : MonoBehaviour {
 
     public static COLOURS colour { set; get; }
 
-    public void GoToShop()
+    void Awake()
     {
+        switch (colour)
+        {
+            case COLOURS.BLUE:
+                text.text = "BLUE BALLS";
+                break;
+            case COLOURS.YELLOW:
+                text.text = "YELLOW BALLS";
+                break;
+            case COLOURS.RED:
+                text.text = "RED BALLS";
+                break;
+            case COLOURS.GREEN:
+                text.text = "GREEN BALLS";
+                break;
+            case COLOURS.ORANGE:
+                text.text = "ORANGE BALLS";
+                break;
+
+        }
+    }
+
+    public void GoToShop(int number)
+    {
+        switch (number)
+        {
+            case 0:
+                colour = COLOURS.BLUE;
+                break;
+            case 1:
+                colour = COLOURS.YELLOW;
+                break;
+            case 2:
+                colour = COLOURS.RED;
+                break;
+            case 3:
+                colour = COLOURS.ORANGE;
+                break;
+            case 4:
+                colour = COLOURS.GREEN;
+                break;
+        }
         menu.SetActive(true);
     }
 
-    public void BuyRedBalls(int amount)
+    public void BuyBalls(int amount)
+    {
+        switch (colour)
+        {
+            case COLOURS.BLUE:
+                BuyBlueBalls(amount);
+                break;
+            case COLOURS.YELLOW:
+                BuyYellowBalls(amount);
+                break;
+            case COLOURS.RED:
+                BuyRedBalls(amount);
+                break;
+            case COLOURS.GREEN:
+                BuyGreenBalls(amount);
+                break;
+            case COLOURS.ORANGE:
+                BuyOrangeBalls(amount);
+                break;
+        }
+    }
+
+    private void BuyRedBalls(int amount)
     {
 
         //IAPManager.Instance.BuyRedBalls(amount);
+        Debug.Log("Buying Red Balls");
     }
 
-    public void BuyBlueBalls(int amount)
+    private void BuyBlueBalls(int amount)
     {
         //IAPManager.Instance.BuyBlueBalls(amount);
+        Debug.Log("Buying Blue Balls");
     }
 
-    public void BuyYellowBalls(int amount)
+    private void BuyYellowBalls(int amount)
     {
         //IAPManager.Instance.BuyYelllowBalls(amount);
+        Debug.Log("Buying Yellow Balls");
     }
 
-    public void BuyOrangeBalls(int amount)
+    private void BuyOrangeBalls(int amount)
     {
         //IAPManager.Instance.BuyOrangeBalls(amount);
+        Debug.Log("Buying Orange Balls");
     }
 
-    public void BuyGreenBalls(int amount)
+    private void BuyGreenBalls(int amount)
     {
         //IAPManager.Instance.BuyGreenBalls(amount);
+        Debug.Log("Buying Green Balls");
     }
 }
